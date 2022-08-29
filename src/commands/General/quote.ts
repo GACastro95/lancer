fceac71f9b3a86ccc1a8dc3575ce8d0150ced6dd
nLspriter <52107943+GACastro95@users.noreply.github.com>
2022-08-29 22:07:39 +0100

    Revert "Update quote command to show replying to"
    This reverts commit 15e9bbe5e670813949ed6006af47e518763a9110.

--- src/commands/General/quote.ts
+++ src/commands/General/quote.ts
@@ -43,6 +43,8 @@ export class UserCommand extends Command {
 			const sticker = message.stickers.first()?.url;
 			var embed;
 
+			console.log(message);
+
 			if (message.embeds.length > 0) {
 				embed = message.embeds[0];
                 console.log(embed)
@@ -64,23 +66,12 @@ export class UserCommand extends Command {
 					.setURL(quote!)
 					.setColor(user.displayColor as ColorResolvable)
 					.setDescription(`${message.content}`)
+					.addFields({ name: `Source`, value: `[jump to message](${quote})`, inline: true })
 					.setImage(sticker ?? attachments[0])
 					.setFooter({
 						text: `${guild.name} • #${channelInfo!.name} • ${date.toLocaleDateString('ja-JP')} at ${hour}:${minutes} UTC`,
 						iconURL: guild!.iconURL()!
 					});
-
-                    if (message.reference) {
-                        var reply = await message.fetchReference();
-                        embed.addFields(
-                            { name: `Replying to`, value: `[${reply.content}](${reply.url})` },
-                            { name: `Source`, value: `[jump to message](${quote})`, inline: true }
-                        )
-                    } else {
-                        embed.addFields(
-                            { name: `Source`, value: `[jump to message](${quote})`, inline: true }
-                        )
-                    }
 			}
 
 			return interaction.reply({ embeds: [embed] });
