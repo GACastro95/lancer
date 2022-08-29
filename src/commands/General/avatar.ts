42d86015eb7a0f820467b49ffa558ee499f85339
nLspriter <52107943+GACastro95@users.noreply.github.com>
2022-08-29 08:23:02 +0100

    Add slash commands to avatar command

--- src/commands/General/avatar.ts
+++ src/commands/General/avatar.ts
@@ -10,17 +10,36 @@ import { ColorResolvable, Message, MessageEmbed } from 'discord.js';
     subcommands: [
         {
             name: 'server',
-            messageRun: 'serverAvatar'
+            messageRun: 'serverAvatarMessage',
+            chatInputRun: 'serverAvatarInteraction'
         },
         {
             name: 'global',
-            messageRun: 'globalAvatar',
+            messageRun: 'globalAvatarMessage',
+            chatInputRun: 'globalAvatarInteraction',
             default: true
         }
     ]
 })
 export class UserCommand extends Subcommand {
-    public async globalAvatar(message: Message) {
+    public override registerApplicationCommands(registry: Subcommand.Registry) {
+        registry.registerChatInputCommand((builder) =>
+            builder
+                .setName(this.name)
+                .setDescription(this.description)
+                .addSubcommand((command) =>
+                    command
+                        .setName('server')
+                        .setDescription('Get the server avatar of the user')
+                )
+                .addSubcommand((command) =>
+                    command
+                        .setName('global')
+                        .setDescription('Get the global avatar of the user'))
+        )
+    }
+
+    public async globalAvatarMessage(message: Message) {
         const user = message.author;
 
         const embed = new MessageEmbed()
@@ -31,7 +50,18 @@ export class UserCommand extends Subcommand {
         return send(message, { embeds: [embed] });
     }
 
-    public async serverAvatar(message: Message) {
+    public async globalAvatarInteraction(interaction: Subcommand.ChatInputInteraction) {
+        const user = interaction.user;
+
+        const embed = new MessageEmbed()
+            .setTitle("Global Avatar of " + user.tag)
+            .setImage(user.displayAvatarURL({ dynamic: true }))
+            .setColor("#5865F2")
+
+        return interaction.reply({ embeds: [embed] });
+    }
+
+    public async serverAvatarMessage(message: Message) {
         const guild = message.guild
         const user = message.author;
         const guildUser = guild?.members.cache.get(user.id)
@@ -42,5 +72,18 @@ export class UserCommand extends Subcommand {
             .setColor(guildUser?.displayColor as ColorResolvable)
 
         return send(message, { embeds: [embed] });
+    }
+
+    public async serverAvatarInteraction(interaction: Subcommand.ChatInputInteraction) {
+        const guild = interaction.guild
+        const user = interaction.user;
+        const guildUser = guild?.members.cache.get(user.id)
+
+        const embed = new MessageEmbed()
+            .setTitle("Server Avatar of " + `${guildUser?.nickname} (${user.tag})` ?? user.tag)
+            .setImage(guildUser?.displayAvatarURL({ dynamic: true }) ?? user.displayAvatarURL({ dynamic: true }))
+            .setColor(guildUser?.displayColor as ColorResolvable)
+
+        return interaction.reply({ embeds: [embed] });
     }
 }
