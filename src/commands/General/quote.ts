9b9a562727b8ee8bbf27e941d1dd1569e3a01cf4
nLspriter <52107943+GACastro95@users.noreply.github.com>
2022-11-25 15:41:17 +0000

    Change quotes for embeds

--- src/commands/General/quote.ts
+++ src/commands/General/quote.ts
@@ -41,47 +41,43 @@ export class UserCommand extends Command {
 			const user = await guild.members.fetch(message.author.id);
 			const attachments = message.attachments.map((e) => e.url);
 			const sticker = message.stickers.first()?.url;
+			var imageURL;
 			var embed;
 
 			if (message.embeds.length > 0) {
 				embed = message.embeds[0];
-                console.log(embed)
-				embed
-					.setTitle(`${embed.author?.name ?? ''}`)
-					.setImage(embed.image?.proxyURL ?? embed.thumbnail?.url ?? '')
-					.setThumbnail(`${embed.author?.proxyIconURL ?? ''}`)
-					.setAuthor({ name: `${user.displayName} (${user.user.tag}) posted...`, iconURL: `${user.displayAvatarURL()}` });
-				if (embed.url) {
-					embed.setFields({ name: `Source`, value: `[go to link](${embed.url})` }).setURL('');
+				if (embed.image != null) {
+					imageURL = embed.image.proxyURL;
+
 				}
-			} else {
-				var date = new Date(message.createdTimestamp);
-				var hour = date.getUTCHours();
-				var minutes = date.getUTCMinutes();
-
-				embed = new MessageEmbed()
-					.setAuthor({ name: `${user.displayName} (${user.user.tag}) said...`, iconURL: `${user.displayAvatarURL()}` })
-					.setURL(quote!)
-					.setColor(user.displayColor as ColorResolvable)
-					.setDescription(`${message.content}`)
-					.setImage(sticker ?? attachments[0])
-					.setFooter({
-						text: `${guild.name} • #${channelInfo!.name} • ${date.toLocaleDateString('ja-JP')} at ${hour}:${minutes} UTC`,
-						iconURL: guild!.iconURL()!
-					});
-
-                    if (message.reference) {
-                        var reply = await message.fetchReference();
-                        embed.addFields(
-                            { name: `Replying to`, value: `[${reply.content}](${reply.url})` },
-                            { name: `Source`, value: `[jump to message](${quote})`, inline: true }
-                        )
-                    } else {
-                        embed.addFields(
-                            { name: `Source`, value: `[jump to message](${quote})`, inline: true }
-                        )
-                    }
 			}
+
+			var date = new Date(message.createdTimestamp);
+			var hour = date.getUTCHours();
+			var minutes = date.getUTCMinutes();
+
+			embed = new MessageEmbed()
+				.setAuthor({ name: `${user.displayName} (${user.user.tag}) said...`, iconURL: `${user.displayAvatarURL()}` })
+				.setURL(quote!)
+				.setColor(user.displayColor as ColorResolvable)
+				.setDescription(`${message.content}`)
+				.setImage(imageURL ?? sticker ?? attachments[0])
+				.setFooter({
+					text: `${guild.name} • #${channelInfo!.name} • ${date.toLocaleDateString('ja-JP')} at ${hour}:${minutes} UTC`,
+					iconURL: guild!.iconURL()!
+				});
+
+				if (message.reference) {
+					var reply = await message.fetchReference();
+					embed.addFields(
+						{ name: `Replying to`, value: `[${reply.content}](${reply.url})` },
+						{ name: `Source`, value: `[jump to message](${quote})`, inline: true }
+					)
+				} else {
+					embed.addFields(
+						{ name: `Source`, value: `[jump to message](${quote})`, inline: true }
+					)
+				}
 
 			return interaction.reply({ embeds: [embed] });
 		} catch (error) {
